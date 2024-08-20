23712960fb7a806aee48fc933284a0c120dcb36f
EskildVeren <eskild.overen@gmail.com>
2024-08-20 20:21:14 +0200

    feat: :sparkles: add 200 lines of functions for returning chess moves

---
+++ chess/javascript/assets/movementRules.js
@@ -0,0 +1,192 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getKnightMoves = exports.getQueenMoves = exports.getKingMoves = exports.getBishopMoves = exports.getRookMoves = void 0;
+var getRookMoves = function () {
+    var up = {
+        xModifier: 0,
+        yModifier: -1,
+        repeating: true,
+    };
+    var down = {
+        xModifier: 0,
+        yModifier: 1,
+        repeating: true,
+    };
+    var left = {
+        xModifier: -1,
+        yModifier: 0,
+        repeating: true,
+    };
+    var right = {
+        xModifier: 1,
+        yModifier: 0,
+        repeating: true,
+    };
+    return [up, down, left, right];
+};
+exports.getRookMoves = getRookMoves;
+var getBishopMoves = function () {
+    var upLeft = {
+        xModifier: -1,
+        yModifier: -1,
+        repeating: true,
+    };
+    var downLeft = {
+        xModifier: -1,
+        yModifier: 1,
+        repeating: true,
+    };
+    var upRight = {
+        xModifier: 1,
+        yModifier: -1,
+        repeating: true,
+    };
+    var downRight = {
+        xModifier: 1,
+        yModifier: 1,
+        repeating: true,
+    };
+    return [upLeft, upRight, downLeft, downRight];
+};
+exports.getBishopMoves = getBishopMoves;
+var getKingMoves = function () {
+    var upLeft = {
+        xModifier: -1,
+        yModifier: -1,
+        repeating: false,
+    };
+    var downLeft = {
+        xModifier: -1,
+        yModifier: 1,
+        repeating: false,
+    };
+    var upRight = {
+        xModifier: 1,
+        yModifier: -1,
+        repeating: false,
+    };
+    var downRight = {
+        xModifier: 1,
+        yModifier: 1,
+        repeating: false,
+    };
+    var up = {
+        xModifier: 0,
+        yModifier: -1,
+        repeating: false,
+    };
+    var down = {
+        xModifier: 0,
+        yModifier: 1,
+        repeating: false,
+    };
+    var left = {
+        xModifier: -1,
+        yModifier: 0,
+        repeating: false,
+    };
+    var right = {
+        xModifier: 1,
+        yModifier: 0,
+        repeating: false,
+    };
+    return [upLeft, upRight, downLeft, downRight, up, down, left, right];
+};
+exports.getKingMoves = getKingMoves;
+var getQueenMoves = function () {
+    var upLeft = {
+        xModifier: -1,
+        yModifier: -1,
+        repeating: true,
+    };
+    var downLeft = {
+        xModifier: -1,
+        yModifier: 1,
+        repeating: true,
+    };
+    var upRight = {
+        xModifier: 1,
+        yModifier: -1,
+        repeating: true,
+    };
+    var downRight = {
+        xModifier: 1,
+        yModifier: 1,
+        repeating: true,
+    };
+    var up = {
+        xModifier: 0,
+        yModifier: -1,
+        repeating: true,
+    };
+    var down = {
+        xModifier: 0,
+        yModifier: 1,
+        repeating: true,
+    };
+    var left = {
+        xModifier: -1,
+        yModifier: 0,
+        repeating: true,
+    };
+    var right = {
+        xModifier: 1,
+        yModifier: 0,
+        repeating: true,
+    };
+    return [upLeft, upRight, downLeft, downRight, up, down, left, right];
+};
+exports.getQueenMoves = getQueenMoves;
+var getKnightMoves = function () {
+    var upLeft = {
+        xModifier: -1,
+        yModifier: -2,
+        repeating: false,
+    };
+    var upRight = {
+        xModifier: 1,
+        yModifier: -2,
+        repeating: false,
+    };
+    var downLeft = {
+        xModifier: -1,
+        yModifier: 2,
+        repeating: false,
+    };
+    var downRight = {
+        xModifier: 1,
+        yModifier: 2,
+        repeating: false,
+    };
+    var lowerLeft = {
+        xModifier: -2,
+        yModifier: 1,
+        repeating: false,
+    };
+    var upperLeft = {
+        xModifier: -2,
+        yModifier: -1,
+        repeating: false,
+    };
+    var lowerRight = {
+        xModifier: 2,
+        yModifier: 1,
+        repeating: false,
+    };
+    var upperRight = {
+        xModifier: 2,
+        yModifier: -1,
+        repeating: false,
+    };
+    return [
+        upLeft,
+        upRight,
+        downLeft,
+        downRight,
+        lowerLeft,
+        upperLeft,
+        lowerRight,
+        upperRight,
+    ];
+};
+exports.getKnightMoves = getKnightMoves;
---
+++ chess/src/assets/movementRules.ts
@@ -0,0 +1,194 @@
+import { movementRule } from "./returnValidMoves";
+
+export const getRookMoves = () => {
+  const up: movementRule = {
+    xModifier: 0,
+    yModifier: -1,
+    repeating: true,
+  };
+  const down: movementRule = {
+    xModifier: 0,
+    yModifier: 1,
+    repeating: true,
+  };
+  const left: movementRule = {
+    xModifier: -1,
+    yModifier: 0,
+    repeating: true,
+  };
+  const right: movementRule = {
+    xModifier: 1,
+    yModifier: 0,
+    repeating: true,
+  };
+
+  return [up, down, left, right];
+};
+
+export const getBishopMoves = () => {
+  const upLeft: movementRule = {
+    xModifier: -1,
+    yModifier: -1,
+    repeating: true,
+  };
+  const downLeft: movementRule = {
+    xModifier: -1,
+    yModifier: 1,
+    repeating: true,
+  };
+  const upRight: movementRule = {
+    xModifier: 1,
+    yModifier: -1,
+    repeating: true,
+  };
+  const downRight: movementRule = {
+    xModifier: 1,
+    yModifier: 1,
+    repeating: true,
+  };
+
+  return [upLeft, upRight, downLeft, downRight];
+};
+
+export const getKingMoves = () => {
+  const upLeft: movementRule = {
+    xModifier: -1,
+    yModifier: -1,
+    repeating: false,
+  };
+  const downLeft: movementRule = {
+    xModifier: -1,
+    yModifier: 1,
+    repeating: false,
+  };
+  const upRight: movementRule = {
+    xModifier: 1,
+    yModifier: -1,
+    repeating: false,
+  };
+  const downRight: movementRule = {
+    xModifier: 1,
+    yModifier: 1,
+    repeating: false,
+  };
+  const up: movementRule = {
+    xModifier: 0,
+    yModifier: -1,
+    repeating: false,
+  };
+  const down: movementRule = {
+    xModifier: 0,
+    yModifier: 1,
+    repeating: false,
+  };
+  const left: movementRule = {
+    xModifier: -1,
+    yModifier: 0,
+    repeating: false,
+  };
+  const right: movementRule = {
+    xModifier: 1,
+    yModifier: 0,
+    repeating: false,
+  };
+
+  return [upLeft, upRight, downLeft, downRight, up, down, left, right];
+};
+
+export const getQueenMoves = () => {
+  const upLeft: movementRule = {
+    xModifier: -1,
+    yModifier: -1,
+    repeating: true,
+  };
+  const downLeft: movementRule = {
+    xModifier: -1,
+    yModifier: 1,
+    repeating: true,
+  };
+  const upRight: movementRule = {
+    xModifier: 1,
+    yModifier: -1,
+    repeating: true,
+  };
+  const downRight: movementRule = {
+    xModifier: 1,
+    yModifier: 1,
+    repeating: true,
+  };
+  const up: movementRule = {
+    xModifier: 0,
+    yModifier: -1,
+    repeating: true,
+  };
+  const down: movementRule = {
+    xModifier: 0,
+    yModifier: 1,
+    repeating: true,
+  };
+  const left: movementRule = {
+    xModifier: -1,
+    yModifier: 0,
+    repeating: true,
+  };
+  const right: movementRule = {
+    xModifier: 1,
+    yModifier: 0,
+    repeating: true,
+  };
+
+  return [upLeft, upRight, downLeft, downRight, up, down, left, right];
+};
+export const getKnightMoves = () => {
+  const upLeft: movementRule = {
+    xModifier: -1,
+    yModifier: -2,
+    repeating: false,
+  };
+  const upRight: movementRule = {
+    xModifier: 1,
+    yModifier: -2,
+    repeating: false,
+  };
+  const downLeft: movementRule = {
+    xModifier: -1,
+    yModifier: 2,
+    repeating: false,
+  };
+  const downRight: movementRule = {
+    xModifier: 1,
+    yModifier: 2,
+    repeating: false,
+  };
+  const lowerLeft: movementRule = {
+    xModifier: -2,
+    yModifier: 1,
+    repeating: false,
+  };
+  const upperLeft: movementRule = {
+    xModifier: -2,
+    yModifier: -1,
+    repeating: false,
+  };
+  const lowerRight: movementRule = {
+    xModifier: 2,
+    yModifier: 1,
+    repeating: false,
+  };
+  const upperRight: movementRule = {
+    xModifier: 2,
+    yModifier: -1,
+    repeating: false,
+  };
+
+  return [
+    upLeft,
+    upRight,
+    downLeft,
+    downRight,
+    lowerLeft,
+    upperLeft,
+    lowerRight,
+    upperRight,
+  ];
+};
