39ccec036cba5de0d395acda322cb7be4c0582bf
EskildVeren <eskild.overen@gmail.com>
2024-09-06 00:17:28 +0200

    feat: :label: add type for blogposts

---
+++ app/src/utilities/blogPost.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+export type BlogPost = {
+  id: number;
+  title: string;
+  introduction: string;
+  paragraphs: string[];
+  conclusion: string;
+};
---
+++ app/src/utilities/mockBlogPosts.ts
@@ -0,0 +1,18 @@
+import { BlogPost } from "./blogPost";
+
+export const mockPosts: BlogPost[] = [
+  {
+    id: 0,
+    title: "Flixbuss: Interrail, men buss",
+    introduction:
+      "Many cant afford holiday. Interrail is also expensive. Use bus",
+    paragraphs: [
+      "Step one: FLy to europe",
+      "Step two: explore city for a few days",
+      "Step three: Take bus to new city",
+      "Repeat until happy",
+      "Fly home in the end",
+    ],
+    conclusion: "It was fun, wasn't it?",
+  },
+];
